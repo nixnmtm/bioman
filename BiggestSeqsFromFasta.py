d9984cf988fff59f110cb5d69c8e6971882febc6
alunem <emmanuel.prestat.pro@gmail.com>
2012-10-08 18:09:47 -0700

    modified:   BiggestSeqsFromFasta.py 	modified:   CountEachElement.py 	modified:   HMMER2KOtable.py 	modified:   SPs2ECs.py 	modified:   SubsetFasta.py 	modified:   blasthits2ECs.py 	modified:   countMotif.py 	modified:   exemple_biopython.py 	modified:   fastqID.py 	modified:   ko2tab.py 	modified:   test.py

--- BiggestSeqsFromFasta.py
+++ BiggestSeqsFromFasta.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
-### 
+### Extracts the N (default is N=100) sequences from a fasta file 
+### needs BioPython
 
 import string
 import sys
+import argparse
 from Bio import SeqIO
 from StringIO import StringIO
 from operator import itemgetter
 
+parser = argparse.ArgumentParser(description="Extracts the N (default is N=100) sequences from a fasta file ")
+parser.add_argument("fastafile", help="the fasta file from which the Nth largest sequences should be extracted")
+parser.add_argument("-N", "--number", default=100, type=int, help="the number of sequences to be extracted")
+parser.add_argument("-o", "--out", default="output.100.fasta", help="output fasta file")
+args = parser.parse_args()
 
-fastafile = sys.argv[1]
-Nseqs = sys.argv[2]
 
+fastafile = args.fastafile
+Nseqs = args.number
+out= args.out
+
+## should add a precaution
+#try:
+#   with open(out) as f: 
+#except IOError as e:
+#   print 'Oh dear.'
+#
 seqs={}
 seqsOrd={}
 
@@ -24,7 +39,7 @@ handle.close()
 seqsOrd = seqs.items()
 seqsOrd.sort(key=itemgetter(1),reverse=True)
 
-handle = open(fastafile+".ord."+Nseqs, "w")
+handle = open(out, "w")
 for i in seqsOrd[0:int(Nseqs)]:
 	SeqIO.write(i[0], handle, "fasta")
 handle.close()
