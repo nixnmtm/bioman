3642f6da14c5efdefe212b579ee728e66d17720f
alunem <emmanuel.prestat.pro@gmail.com>
2013-03-22 15:17:58 -0700

    modified:   ktable2umx.py
    some errors removed

--- ktable2umx.py
+++ ktable2umx.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser(description="""
 Open a kmer tsv (arg 1) (e.g. fasta2ktable output) and format it in a <tab> seperated
 U-matrix file (.umx). Moreover, it removes columns which contains 0 only and write row
-names and k-mer names in separated files (.seqnames and .kmernames)
+names and k-mer names in separated files (.names and .kmernames)
 """)
 parser.add_argument("ktable", help="a ktable file")
 parser.add_argument("-s", "--separator", default = "\t", help="the separator to make the output table, default is <tab>")
@@ -16,14 +16,13 @@ args = parser.parse_args()
 ktable = args.ktable
 sep = args.separator
 
-ktable="/home/manu/Projects/Yujie/JC_06.tetra.tsv"
 kt = pd.DataFrame.from_csv(ktable, sep='\t', header=0)
 
 ## removing columns with 0 only
 ktsub = kt[kt.columns[(kt != 0).any()]]
 
 ## writing rows in file
-with open (ktable+".seqnames", 'w') as frows:
+with open (ktable+".names", 'w') as frows:
     for row in ktsub.index:
         frows.write(row+"\n")
 
