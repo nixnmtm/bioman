e8ef0cfa3ceb6b37a55f691e107cb4bb56528dbd
Emmanuel Prestat <emmanuel.prestat.pro@gmail.com>
2013-09-20 04:38:39 -0700

    modified:   sdiff.py help section added

--- sdiff.py
+++ sdiff.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # output elements in list 1 than are not in list 2
-
 import sys
+import argparse
 
-f1=sys.argv[1]
-f2=sys.argv[2]
+parser = argparse.ArgumentParser(description="output elements in list 1 <file1> than are not in list 2 <file2>, i.e. elements that are present ONLY in file 1. Each item in the text files are separated with a newline")
+parser.add_argument("file1", help="the file that contains list of items to be tested for unique presence, i.e. which are not in file2")
+parser.add_argument("file2", help="the file that contains list of items to be suppressed from file 1")
+args = parser.parse_args()
+
+f1=args.file1
+f2=args.file2
 
 l1=open(f1)
 l2=open(f2)
